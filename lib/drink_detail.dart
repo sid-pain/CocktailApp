128b13dff880b7b14395efa56f628a1d131bfc63
sid-pain <sid.khedekar100@gmail.com>
2021-05-29 15:34:56 +0530

    commit

---
+++ lib/drink_detail.dart
@@ -0,0 +1,57 @@
+import 'package:cocktail/main.dart';
+import 'package:flutter/material.dart';
+
+class DrinkDetail extends StatelessWidget {
+  final drink;
+
+  const DrinkDetail({Key? key, @required this.drink}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [myColor, Colors.lightBlueAccent])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(drink["strDrink"]),
+          elevation: 0.0,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: drink["idDrink"],
+                child: CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: NetworkImage(drink["strDrinkThumb"]),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "Id: ${drink["idDrink"]}",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "${drink["strDrink"]}",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
